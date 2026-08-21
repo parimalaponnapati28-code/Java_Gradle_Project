9972c6624edc1b6cdaa2da9d58fa63d8123a386b
Parimala <parimalaponnapati28@gmail.com>
2026-08-21 15:58:31 +0530

    Final clean project

--- Task-3 Gradle/app/src/main/java/org/example/App.java
+++ Task-3 Gradle/app/src/main/java/org/example/App.java
@@ -1,13 +1,10 @@
 package org.example;
 
 public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
-        System.out.println("=================================");
-        System.out.println("    CodeAlpha Java Gradle App");
-        System.out.println("=================================");
-        System.out.println("Build Tool : Gradle 9.6.1");
-        System.out.println("Language   : Java 21");
-        System.out.println("IDE        : IntelliJ IDEA");
-        System.out.println("Status     : Application Running Successfully");
+        System.out.println(new App().getGreeting());
     }
 }
